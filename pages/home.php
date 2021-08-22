03d63121e360feba4dfd19e9bceb32c7c3734f71
Ronildo Ferreira <ronildo.ufpi@gmail.com>
2021-08-21 23:53:48 -0300

    mudanças

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,39 @@
-<?php
+<html>
+<head>
+<title>Loja de Doces</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class = "topo">
+
+        <div class="topo1"></div>
+
+        <div class="banner"></div>
+</dibv>        
+<div class="menu">
+<ul>
+<a href=""><li>Home</li></a>
+<a href=""><li>Produtos</li></a>
+<a href=""><li>Contato</li></a>
+</ul>
+
+</div>
 
 
+<div class="container">
+<div class="home_sobre"> 
 
-?>
+
+<img src="imagens/img02.jpg" border="0" width="200" heigth="200"/>
+<h4>Sobre<h4>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</div>
+
+</div>
+
+
+<div class="rodape"></div>
+</body>
+
+</html>
